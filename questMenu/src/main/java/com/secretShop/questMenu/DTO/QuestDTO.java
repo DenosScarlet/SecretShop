ccd3a539e91ad8ec59c7c26b4e8be7a1bd03c2e8
Давыдов Денис <dienis.davydov.2003@mail.ru>
2025-04-14 17:36:57 +0300

    feature/QuestMenu/Дополнил DTO

--- questMenu/src/main/java/com/secretShop/questMenu/DTO/QuestDTO.java
+++ questMenu/src/main/java/com/secretShop/questMenu/DTO/QuestDTO.java
@@ -2,17 +2,24 @@ package com.secretShop.questMenu.DTO;
 
 import com.secretShop.questMenu.enums.Frequency;
 import com.secretShop.questMenu.enums.WorkGroup;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestDTO {
-    private String questTitle;
+    private UUID quest_id;
+    private String quest_title;
     private String description;
-    private Frequency Frequency;
-    private WorkGroup workGroup;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private Integer steps_to_complete;
+    private Frequency frequency;
+    private WorkGroup work_group;
+    private LocalDateTime start_date;
+    private LocalDateTime end_date;
     private Integer cost;
 }
