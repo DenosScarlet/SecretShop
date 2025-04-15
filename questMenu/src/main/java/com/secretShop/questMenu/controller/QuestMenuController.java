1d585c869b1e105dbe5b214f4ea1bb3ec80fd594
Давыдов Денис <dienis.davydov.2003@mail.ru>
2025-04-15 13:28:10 +0300

    feature/QuestMenu/Добавил методы контроллеру и рест клиент

--- questMenu/src/main/java/com/secretShop/questMenu/controller/QuestMenuController.java
+++ questMenu/src/main/java/com/secretShop/questMenu/controller/QuestMenuController.java
@@ -1,7 +1,43 @@
 package com.secretShop.questMenu.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.secretShop.questMenu.DTO.QuestDTO;
+import com.secretShop.questMenu.service.QuestClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/quest")
+@RequiredArgsConstructor
 public class QuestMenuController {
+
+    private final QuestClient questClient = new QuestClient();
+
+    @GetMapping
+    public List<QuestDTO> getAllQuests() {
+        return questClient.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public QuestDTO getQuestById(@PathVariable("id") UUID id) {
+        return questClient.findById(id);
+    }
+
+    @PostMapping
+    public QuestDTO createQuest(@RequestBody QuestDTO quest) {
+        return questClient.save(quest);
+    }
+
+    @PutMapping("/{id}")
+    public QuestDTO updateQuest(@PathVariable("id") UUID id, @RequestBody QuestDTO quest) {
+        return questClient.save(quest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteQuest(@PathVariable("id") UUID id) {
+        questClient.delete(id);
+    }
+
 }
